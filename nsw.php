60b3fa527c1b7eabdfa2f114a655198f6164bc2f
changwoo <ep6tri@hotmail.com>
2021-04-08 14:20:13 +0900

    1.0.1 커밋

--- nsw.php
+++ nsw.php
@@ -6,7 +6,7 @@
  * Author URI:   mailto://chwnam@gmail.com
  * Plugin URI:   https://github.com/chwnam/naran-screen-info
  * Requires PHP: 7.2
- * Version:      1.0.0
+ * Version:      1.0.1
  * License:      GPLv2 or later
  */
 
@@ -15,5 +15,11 @@ function nsw_enqueue_scripts() {
 	wp_enqueue_style( 'nsw', plugins_url( 'nsw.css', __FILE__ ), [], '1.0.0' );
 }
 
-add_action( 'wp_enqueue_scripts', 'nsw_enqueue_scripts', 200 );
-add_action( 'admin_enqueue_scripts', 'nsw_enqueue_scripts', 200 );
+function nsw_initialize() {
+	if ( is_user_logged_in() && is_admin_bar_showing() ) {
+		add_action( 'wp_enqueue_scripts', 'nsw_enqueue_scripts', 200 );
+		add_action( 'admin_enqueue_scripts', 'nsw_enqueue_scripts', 200 );
+	}
+}
+
+add_action( 'init', 'nsw_initialize' );
